fcf43e338a93ba50da19ad43b2d2be692762577e
Nick Ford <nford.work@gmail.com>
2017-05-24 16:20:08 +0100

    Blog formatting complete. Repeatable image galary built. Custom blog CSS

--- nf_rev2/blog.php
+++ nf_rev2/blog.php
@@ -1,149 +1,202 @@
 <?php require_once( 'couch/cms.php' ); ?>
 <cms:template title='Blog' clonable='1' commentable='1'>
-	<cms:editable name='blog_content' type='richtext' />
-	
-	<cms:editable name='blog_image'
-		crop='1'
-		width='610'
-		height='150'
-		type='image'
-	/>
-	
-	<cms:folder name="philosophy" title="Philosophy" />
-	<cms:folder name="history" title="History" />
-	<cms:folder name="jobs" title="Jobs" />
-	<cms:folder name="staff" title="Staff" />
-	<cms:folder name="clients" title="Clients" />
-	
+<cms:editable name='blog_content' type='richtext' />
+        <cms:repeatable name='my_blog_images'>
+               <cms:editable type='image' name='blog_image' label='Photo' />
+        </cms:repeatable>
+
+<cms:folder name="brewing" title="Brewing" />
+<cms:folder name="programming" title="Programming" />
+<cms:folder name="pi-arduino" title="RasPi - Arduino" />
+<cms:folder name="web-dev" title="Web Dev" />    
 </cms:template>
 
 <cms:if k_is_page >
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
 <head>
-	<title>Aurelius | Blog</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	
-	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/reset.css" />
-	<link rel="stylesheet" href="css/styles.css" />
+    <title>Nick Ford - Web Developer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:500,300,700,800' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" type="../image/icp" href="images/favicon.ico"/>
+    <link href="http://www.nick-ford.com/dev/css/style.css" rel="stylesheet">
+    <link href="http://www.nick-ford.com/dev/css/font-awesome.min.css" rel="stylesheet" >
 
-	<!-- Scripts -->
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
-	<!--[if IE 6]>
-	<script src="js/DD_belatedPNG_0.0.8a-min.js"></script>
-	<script>
-	  /* EXAMPLE */
-	  DD_belatedPNG.fix('.button');
-	  
-	  /* string argument can be any CSS selector */
-	  /* .png_bg example is unnecessary */
-	  /* change it to what suits you! */
-	</script>
-	<![endif]-->
-	
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body>
+<div class="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="nav-section">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html">NICK<strong>FORD</strong></a>
+    </div>
 
-	<div id="wrapper" class="container_12 clearfix">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.html#about">About</a></li>
+        <li><a href="index.html#portfolio">Portfolio</a></li>
+        <li><a href="index.html#skills">Skills</a></li>
+        <li><a href="index.html#contact">Contact</a></li>
+        <li><a href="blog.html">Blog</a></li>
+    </ul>  
+</div><!-- /.navbar-collapse -->
+</div> <!-- /.container-fluid -->
+</nav>
+</div>
 
-		<!-- Text Logo -->
-		<h1 id="logo" class="grid_4">Aurelius</h1>
-		
-		<!-- Navigation Menu -->
-		<ul id="navigation" class="grid_8">
-			<li><a href="contact.html"><span class="meta">Get in touch</span><br />Contact Us</a></li>
-			<li><a href="blog.html" class="current"><span class="meta">Latest news</span><br />Blog</a></li>
-			<li><a href="portfolio.html"><span class="meta">Our latest work</span><br />Portfolio</a></li>
-			<li><a href="about.html"><span class="meta">Who are we?</span><br />About</a></li>
-			<li><a href="index.html"><span class="meta">Homepage</span><br />Home</a></li>
-		</ul>
-		
-		<div class="hr grid_12 clearfix">&nbsp;</div>
-			
-		<!-- Caption Line -->
-		<h2 class="grid_12 caption clearfix">Our <span>blog</span>, keeping you up-to-date on our latest news.</h2>
-		
-		<div class="hr grid_12 clearfix">&nbsp;</div>
-		
-		<!-- Column 1 /Content -->
-		<div class="grid_8">
-			
-			<!-- Blog Post -->
-			<div class="post">
-				<!-- Post Title -->
-				<h3 class="title"><a href="<cms:show k_page_link />"><cms:show k_page_title /></a></h3>
-				<!-- Post Title -->
-				<cms:if k_page_foldertitle >
-					<cms:set my_category=k_page_foldertitle />
-				<cms:else />
-					<cms:set my_category='Uncategorised' />
-				</cms:if>
-				<p class="sub"><a href="#"><cms:show my_category /></a> &bull; <cms:date k_page_date format='jS M, y'/> &bull; <a href="#"><cms:show k_comments_count /> Comments</a></p>
-				<div class="hr dotted clearfix">&nbsp;</div>
-				<!-- Post Title -->
-				<img class="thumb" src="<cms:show blog_image />" alt=""/>
-				<!-- Post Content -->
-				<cms:show blog_content />
-				<!-- Post Links -->
-				<p class="clearfix">
-					<a href="<cms:link masterpage='blog.php' />" class="button float" >&lt;&lt; Back to Blog</a>
-					<a href="#commentform" class="button float right" >Discuss this post</a>
-				</p>
-			</div>
-			<div class="hr clearfix">&nbsp;</div>
-			
-			<!-- Comment's List -->
-			<h3>Comments</h3>
-			<div class="hr dotted clearfix">&nbsp;</div>
-			
-			<cms:if k_comments_count >
-			<ol class="commentlist">
-				<cms:comments page_id=k_page_id order='asc' limit='5' paginate='1' >
-				<li class="comment"> 
-					<div class="gravatar"> 
-						<cms:gravatar email="<cms:show k_comment_author_email />" size="60" />
-						<a name="<cms:show k_comment_anchor />">
-					</div> 
-					<div class="comment_content"> 
-						<div class="clearfix">
-							<cite class="author_name"><a href=""><cms:show k_comment_author /></a></cite>       
-							<div class="comment-meta commentmetadata"><cms:date k_comment_date format='F j, Y'/> at <cms:date k_comment_date format='h:ia'/></a></div> 
-						</div>
-						<div class="comment_text"> 
-							<p><cms:show k_comment /></p> 
-						</div> 
-					</div> 
-				</li> 
-				<cms:paginator />
-				</cms:comments>
-			</ol> 
-			<cms:else />
-				No comments.
-			</cms:if>
-			
-			<div class="hr clearfix">&nbsp;</div>
-			
-			<!-- Comment Form -->
-			<cms:embed 'comments_form.html' />
+<div class="bottom-wrapper anchor">
 
-		</div>
-	
-		<!-- Column 2 / Sidebar -->
-		<cms:embed 'blog_sidebar.html' />
-		
-		<!-- Footer -->
-		<p class="grid_12 footer clearfix">
-			<span class="float"><b>&copy; Copyright</b> <a href="">QwibbleDesigns</a> - remove upon purchase.</span>
-			<a class="float right" href="#">top</a>
-		</p>
-	</div><!--end wrapper-->
+    <div class="section blue-bg">
+        <div class="section-content no-bott-pad blog-header">
+            <div class="title">
+                <h1>Blog</h1>
+            </div> 
+
+
+<!--             <div class="content">
+                <div>
+                </div> -->
+
+            </div>     
+        </div>
+    </div>
+
+    <div class="section white-bg anchor">
+
+        <div class="section-content">
+            <div class="title" id="blog-title">
+                <h1><a style="color: inherit;" href="<cms:show k_page_link />"><cms:show k_page_title /></a></h1>
+                <cms:if k_page_foldertitle >
+                    <cms:set my_category=k_page_foldertitle />
+                        <cms:else />
+                    <cms:set my_category='Uncategorised' />
+                </cms:if>  
+                <span class="tag"><cms:show my_category /></span>
+                <span class="tag"><cms:date k_page_date format='jS M, y'/></span>
+            </div>
+
+            <div class="content content-piece" id="blog-content-large">
+                <div class="sub-cont">
+                    <cms:show blog_content />
+
+                    <div class="galleria">
+                    <cms:show_repeatable 'my_blog_images'>
+                        <img src="<cms:show blog_image/>">
+                    </cms:show_repeatable>
+                    </div>
+
+                    <!-- Post Links -->
+                    <p class="clearfix">
+                        <span class="tag"><a href="<cms:link masterpage='blog.php' />" class="button float" >Back to Blog</a></span>
+                        <span class="tag"><a href="#comments" ><cms:show k_comments_count /> Comments</a></span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="section white-bg anchor">
+
+        <div class="section-content">
+            <div class="title">
+                <h1>Categories</h1>
+            </div>
+
+            <div class="content content-piece">
+                <div class="sub-cont">
+                    <cms:folders masterpage='blog.php' >
+                        <span class="tag"><a href="<cms:show k_folder_link />"><cms:show k_folder_title /></a></span>
+                    </cms:folders>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="section blue-bg anchor" id="comments">
+    <div class="section-content no-bott-pad">
+        <div class="title">
+            <h1>Comments</h1>
+        </div> 
+        <div class="content">
+            <cms:if k_comments_count >
+                <ol class="commentlist">
+                    <cms:comments page_id=k_page_id order='asc' limit='5' paginate='1' >
+                    <li class="comment"> 
+                        <div class="gravatar"> 
+                            <!-- <a name="<cms:show k_comment_anchor />"> -->
+                        </div> 
+                        <div class="comment_content"> 
+                            <div class="clearfix">
+                                <cite class="author_name"><a href=""><cms:show k_comment_author /></a></cite> //        
+                                <cite class="comment-meta commentmetadata"><cms:date k_comment_date format='F j, Y'/> at <cms:date k_comment_date format='h:ia'/></a></cite> 
+                            </div>
+                            <div class="comment_text"> 
+                                <p><cms:show k_comment /></p> 
+                            </div> 
+                        </div> 
+                    </li> 
+                    <cms:paginator />
+                    </cms:comments>
+                </ol> 
+            <cms:else />
+            No comments.
+            </cms:if>
+            <!-- Comment Form -->
+            <cms:embed 'comments_form.html' />
+        </div>
+    </div>     
+</div>
+
+
+
+<div class="section white-bg">
+    <div class="section-content">
+        <div class="content" id="contact">
+            <div>
+                <h2 class="email">
+                </h2><br/>
+                <div id="social">
+                    <a target="_blank" href="mailto:nford.work@gmail.com" class="btn-rounded-blue"><i class="fa fa-envelope-o"></i></a>
+                    <a target="_blank" href="https://github.com/nford88" class="btn-rounded-blue"><i class="fa fa-github"></i></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/nickford2" class="btn-rounded-blue"><i class="fa fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><!-- ./contact -->
+
+<div class="footer section anchor">
+    <div class="section-content">
+        <a href="index.html"><p>NICK<strong>FORD</strong></p></a>
+    </div>
+</div><!-- ./about -->
+</div><!-- bottom wrapper end -->
+
+
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="../../js/galleria/galleria-1.5.7.min.js"></script>
+
+<script>
+(function() { 
+    Galleria.loadTheme('../../js/galleria/themes/classic/galleria.classic.min.js');
+    Galleria.run('.galleria');
+}());
+</script>
 
 </body>
 </html>
 <cms:else />
-	<cms:embed 'blog_list.html' />
+<cms:embed 'blog_list.html' />
 </cms:if>
 
 <?php COUCH::invoke(); ?>
